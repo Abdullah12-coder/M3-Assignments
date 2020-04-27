9ad24ca79e237d5ff7cd9acd445992d444664dc3
Abdullah12-coder <61451596+Abdullah12-coder@users.noreply.github.com>
2020-04-27 13:13:43 +0530

    Add files via upload

---
+++ Spring/Lab1_4/src/EmployeeService.java
@@ -0,0 +1,5 @@
+
+
+public interface EmployeeService {
+public Employee getDetails(int empId);
+}
---
+++ Spring/Lab1_4/src/EmployeeServiceImpl.java
@@ -0,0 +1,10 @@
+
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	public Employee getDetails(int empId) {
+		EmployeeDAO emp=new EmployeeDAO();
+		return emp.getDetails(empId);
+	}
+ 
+}
